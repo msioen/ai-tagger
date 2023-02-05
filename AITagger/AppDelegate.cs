03b3f3a35f1e55ba99c6d643119b0573a5a4ccba
Michiel Sioen <michiel_sioen@hotmail.com>
2023-02-05 10:50:40 +0100

    Project scaffold

---
+++ AITagger/AppDelegate.cs
@@ -0,0 +1,43 @@
+﻿using AppKit;
+using Foundation;
+using ObjCRuntime;
+
+namespace AITagger
+{
+    [Register ("AppDelegate")]
+    public class AppDelegate : NSApplicationDelegate
+    {
+        NSStatusItem _statusItem = NSStatusBar.SystemStatusBar.CreateStatusItem(NSStatusItemLength.Square);
+
+        public AppDelegate ()
+        {
+        }
+
+        public override void DidFinishLaunching (NSNotification notification)
+        {
+            // setup menu
+            _statusItem.Button.Image = NSImage.ImageNamed("StatusBarIcon");
+
+            var menu = new NSMenu();
+
+            //var scanSelectItem = new NSMenuItem("Scan - select", OnScanSelect);
+            //scanSelectItem.BindHotKey(NSUserDefaultsController.SharedUserDefaultsController, Constants.KEY_HOTKEY_SCAN_SELECT);
+            //menu.AddItem(scanSelectItem);
+
+            //var scanClickItem = new NSMenuItem("Scan - click", OnScanClick);
+            //scanClickItem.BindHotKey(NSUserDefaultsController.SharedUserDefaultsController, Constants.KEY_HOTKEY_SCAN_CLICK);
+            //menu.AddItem(scanClickItem);
+
+            //menu.AddItem(NSMenuItem.SeparatorItem);
+            //menu.AddItem(new NSMenuItem("Preferences", OnPreferences));
+            //menu.AddItem(new NSMenuItem("Check for updates", OnCheckForUpdates));
+            //menu.AddItem(new NSMenuItem("Quit", OnQuit));
+            _statusItem.Menu = menu;
+        }
+
+        public override void WillTerminate (NSNotification notification)
+        {
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,15 @@
+# AI Tagger
+
+![Icon](./resources/brain-colored-128.png)
+![Icon](./resources/brain-128.png)
+
+AI Tagger is a macOS utility that can automatically tag images based on AI detected content.
+
+## Release notes
+
+v1.0
+
+
+## Attributions
+
+Icons made by [Freepik - Flaticon](https://www.flaticon.com/authors/freepik).
